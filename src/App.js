fd91d05428a7fc8b81a62735b415bdbb79af2183
Siwa <siwa.boonpun@gmail.com>
2018-12-12 20:17:15 +0700

    first version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.key = "uBCg2t7fJBwD0nr:F7b6F7IkHT0gfbcnuXUQyAB2N";
+    this.url = "https://api.netpie.io/topic/lightforlife/test/";
+    this.state = {
+      light: 0,
+      state: "Manual"
+    };
+    axios
+      .put(`${this.url}state?auth=${this.key}&retain`, 0, {
+        headers: { "Content-Type": "text/plain" }
+      })
+      .then(res => {
+        console.log(res);
+      });
+  }
+
+  sendState = state => {
+    axios
+      .put(`${this.url}state?auth=${this.key}&retain`, state, {
+        headers: { "Content-Type": "text/plain" }
+      })
+      .then(res => {
+        console.log(`${this.url}state?auth${this.key}&retain`);
+        console.log(res);
+      });
+  };
+
+  sendValue = value => {
+    axios
+      .put(`${this.url}value?auth=${this.key}&retain`, this.state.light, {
+        headers: { "Content-Type": "text/plain" }
+      })
+      .then(res => {
+        console.log(`${this.url}value?auth${this.key}&retain`);
+        console.log(res);
+      });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <img
+          src="./light.png"
+          alt="light"
+          style={{ opacity: `${this.state.light / 100}` }}
+          width="300"
+          height="300"
+        />
+        <h3> State : {this.state.state} </h3>
+        <h3> Light : {this.state.light} </h3>
+        <br />
+        <input
+          id="manual"
+          type="range"
+          value={this.state.light}
+          min="0"
+          max="100"
+          onInput={e => {
+            this.setState({ light: e.target.value });
+            this.sendValue(e.target.value);
+          }}
+        />
+        <br />
+        <br />
+        <button
+          onClick={() => {
+            console.log("AutoDim");
+            this.setState({ state: "AutoDim" });
+            this.sendState(1);
+            document.getElementById("manual").disabled = true;
+          }}
+        >
+          Auto Dim
+        </button>
+        &nbsp;
+        <button
+          onClick={() => {
+            console.log("Manual");
+            this.setState({ state: "Manual" });
+            this.sendState(0);
+            document.getElementById("manual").disabled = false;
+          }}
+        >
+          Manual
+        </button>
       </div>
     );
   }
