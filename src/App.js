d7cf9f8c1d34796efcd62d10f67f56e139460bff
Siwa <siwa.boonpun@gmail.com>
2018-12-12 20:37:48 +0700

    fixed bug

--- src/App.js
+++ src/App.js
@@ -27,7 +27,6 @@ class App extends Component {
         headers: { "Content-Type": "text/plain" }
       })
       .then(res => {
-        console.log(`${this.url}state?auth${this.key}&retain`);
         console.log(res);
       });
   };
@@ -38,7 +37,6 @@ class App extends Component {
         headers: { "Content-Type": "text/plain" }
       })
       .then(res => {
-        console.log(`${this.url}value?auth${this.key}&retain`);
         console.log(res);
       });
   };
